2819a4dc31eaebb9a49393d99485da510e4c0284
yukiroazn <yukiro.azn@live.com>
2022-12-18 10:07:43 +0100

    Update

--- front/js/index.js
+++ front/js/index.js
@@ -1,19 +1,16 @@
-// Je demande à fetch de récupérer les données depuis l'url de l'API
+// Récupérer les données depuis l'url de l'API
 fetch('http://localhost:3000/api/products').then((data) => {
 
-// Première promesse .then qui va récupérer la réponse en la transformant en json pour faciliter l'intérprétation par le navigateur
 return data.json();
 }).then((completedata) => {
   
 // Pour ma variable product de ma promise
 let products="";
 
-// J'utilise la méthode map() pour prendre un tableau de nombres et doubler leurs valeurs
 completedata.map((productLink)=>{
 
 // Le + sert à ajouter tous les éléments tant qu'il y en a
-// Je mets le container de mon produit pour simplifier les choses
-// En mettant dollar+accolades est une nouvelle méthode qui permet de directemet combiner des variables et des clés dans un objet ou tableau
+// Copier le code HTML en mettant dollat+accolade
 products+=
 
 `<a href="./product.html?id=${productLink._id}">
@@ -26,11 +23,11 @@ products+=
 
 });
 
-// Je déclare ma variable en mettant l'id de mon container de produits
+// Déclaration
 document.getElementById("items").innerHTML=products;
 
 
-// J'ajoute un message erreur au cas où le serveur ne répond pas
+// Message d'erreur
 }).catch((err)=>{
   console.log(err);
 })
